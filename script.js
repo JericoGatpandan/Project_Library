a6b854887c5d976165db71ce7561ac86473a9227
JericoGatpandan <jericogatpandan0905@gmail.com>
2025-06-11 21:23:15 +0800

    still trying to figure out how to get form input

--- script.js
+++ script.js
@@ -6,21 +6,31 @@ function Book(name, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+const form = document.getElementById("form");
+let nameInput = prompt("Name");
+let authorInput = prompt("Author");
+let pagesInput = prompt("Pages");
+let readInput = prompt("Read");
 
-const addBook = document.getElementById("addBook");
-const nameInput = document.getElementById("name");
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 function addBookToLibrary(name, author, pages, read) {
   const newBook = new Book(name, author, pages, read);
   myLibrary.push(newBook);
 }
 
+addBookToLibrary(nameInput, authorInput, pagesInput, readInput);
 
-addBook.addEventListener("click", function () {
-  addBookToLibrary("Jerico", "hahaha", 12, true);
-  displayBook();
-});
+// addBook.addEventListener("click", () => {
+//   addBookToLibrary(
+//     nameInput.value,
+//     authorInput.value,
+//     pagesInput.value,
+//     readInput.value
+//   );
+// });
 
 function displayBook() {
   for (let index = 0; index < myLibrary.length; index++) {
@@ -28,3 +38,5 @@ function displayBook() {
     console.log(element);
   }
 }
+
+displayBook();
