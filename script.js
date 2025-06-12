65175c8bb6ffe808d57dbdcf8f1eae90998d3eee
JericoGatpandan <jericogatpandan0905@gmail.com>
2025-06-12 12:15:13 +0800

    add function to get the value of the input in the form

--- script.js
+++ script.js
@@ -7,17 +7,7 @@ function Book(name, author, pages, read) {
   this.read = read;
   this.id = crypto.randomUUID();
   this.info = function () {
-    return (
-      this.id +
-      " " +
-      this.name +
-      " " +
-      this.author +
-      " " +
-      this.pages +
-      " " +
-      this.read
-    );
+    return this.name + " " + this.author + " " + this.pages + " " + this.read;
   };
 }
 
@@ -26,14 +16,42 @@ function addBookToLibrary(name, author, pages, read, id) {
   myLibrary.push(newBook);
 }
 
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
+const addBookBtn = document.getElementById("addBook");
+const dialog = document.getElementById("dialog");
+const outputBox = document.querySelector("output");
+const confirmBtn = document.querySelector("#confirmBtn");
 
-showButton.addEventListener("click", () => {
+const titleValue = document.querySelector(".title");
+const authorValue = document.querySelector(".author");
+const pagesValue = document.querySelector(".pages");
+const readValue = document.querySelector(".read");
+
+const Library = document.querySelector(".Library");
+
+addBookBtn.addEventListener("click", () => {
+  titleValue.value = "";
+  authorValue.value = "";
+  pagesValue.value = "";
+  readValue.checked = false;
   dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary(
+    titleValue.value,
+    authorValue.value,
+    pagesValue.value,
+    readValue.checked ? "Read" : "Not Read"
+  );
+  displayBook();
   dialog.close();
 });
+
+function displayBook() {
+  for (let index = 0; index < myLibrary.length; index++) {
+    console.log(myLibrary[index]);
+  }
+}
+
+
